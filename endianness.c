b4ae85714c83962cb3e83a9d8e07504a99b53b65
Tenzin <124920785+Tenzinyo@users.noreply.github.com>
2025-09-10 14:59:37 -0700

    edit endiannes and fix errors

--- endianness.c
+++ endianness.c
@@ -4,7 +4,7 @@ int main(){
     int* xp = &x;
     short xs0,xs1; //16 bit integer
 
-    xs = (short)(*xp); //casts the dereferenced value to a short type and assigns it to variable
+    xs0 = (short)(*xp); //casts the dereferenced value to a short type and assigns it to variable
     xs1 = *((short*)xp); //dereferences this cast pointer to get the short value at the memory location xp
     // little-endian: xs0 ==xs1 , gives consistent results
     // big-endian : xs1==0
