d518586e00976e2dc6fda1317474339f0709c479
Tenzin <124920785+Tenzinyo@users.noreply.github.com>
2025-09-10 14:52:35 -0700

    adding additional functions for boolean and endiannes

---
+++ endianness.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int x = 0x00000123;
+    int* xp = &x;
+    short xs0,xs1; //16 bit integer
+
+    xs = (short)(*xp); //casts the dereferenced value to a short type and assigns it to variable
+    xs1 = *((short*)xp); //dereferences this cast pointer to get the short value at the memory location xp
+    // little-endian: xs0 ==xs1 , gives consistent results
+    // big-endian : xs1==0
+}
