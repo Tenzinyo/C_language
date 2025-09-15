c9dc5d879d5e61724ec7c1e83c848294bc7e81f8
Tenzin <124920785+Tenzinyo@users.noreply.github.com>
2025-09-15 15:12:02 -0700

    sum function in c

--- sum.c
+++ sum.c
@@ -1,5 +1,15 @@
 long plus(long x, long y);
 void sumstore(long x, long y, long *dest){
     long t = plus(x,y);
-    *dest = t;
+    *dest = t; //memory is the dest in c
 }
+
+// sumstore x86-64 assembly
+// pushq  %rbx
+// movq  %rdx,  %rbx
+// call  plus
+    // directly accessing memory out of the stack, moving from register rax to mem loc pointed to by rbx
+// movq  %rax, (%rbx) 
+    // rax is place that the return value was in the function
+// popq  %rbx // pop to execute a return func
+// ret  
